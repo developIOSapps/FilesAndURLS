8675f38b2040d53b2dbeaad7a937a22be78624ce
Steven Hertz <hertzsteven@icloud.com>
2018-10-21 09:17:00 -0400

    working version before catching errors

--- FilesAndURLS/ViewController.swift
+++ FilesAndURLS/ViewController.swift
@@ -13,9 +13,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        get the path to the place for files
+        // get the path to the place for files
         
         guard let thePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { fatalError() }
+        
+        let urlToWriteRead = thePath.appendingPathComponent("note.txt")
+        
+        print(urlToWriteRead.absoluteString)
+        
+        
+        
+        // read file
+        
+        let fileContents = try! String(contentsOf: urlToWriteRead)
+        print("This is the contents of the file\n\(fileContents)")
+        
+        
+        
+        
+        
+        // write file
+        
+        let textToSave =  "hello"
+
+        try! textToSave.write(to: urlToWriteRead, atomically: true, encoding: .utf8)
         
         
         
