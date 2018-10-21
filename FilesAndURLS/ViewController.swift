ecc989ea9a82b6f947ba1a6e3fd0b2fabe040f1c
Steven Hertz <hertzsteven@icloud.com>
2018-10-21 08:37:13 -0400

    Initial Commit

---
+++ FilesAndURLS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilesAndURLS
+//
+//  Created by Steven Hertz on 10/21/18.
+//  Copyright © 2018 DevelopItSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
