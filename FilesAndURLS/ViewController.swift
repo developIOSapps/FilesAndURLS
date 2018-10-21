af05d7557e26cc7872b5e10292c84f24d423a60c
Steven Hertz <hertzsteven@icloud.com>
2018-10-21 08:42:34 -0400

    Commit after guard statement

--- FilesAndURLS/ViewController.swift
+++ FilesAndURLS/ViewController.swift
@@ -12,7 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+//        get the path to the place for files
+        
+        guard let thePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { fatalError() }
+        
+        
+        
     }
 
 
